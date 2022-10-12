7246c63a7c775bd7a98073f38c8d0066b9f87842
Zhiqi Lu <zhiqilu8470@gmail.com>
2022-10-12 20:44:02 +0800

    test: integration test for getting shoppingCart with 2 items

--- src/test/java/com/thoughtworks/capability/web/ShoppingCartControllerTest.java
+++ src/test/java/com/thoughtworks/capability/web/ShoppingCartControllerTest.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.capability.web;
 
+import com.thoughtworks.capability.WebApplicationTest;
+import com.thoughtworks.capability.domain.Product;
 import com.thoughtworks.capability.service.ShoppingCartService;
 import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
 import org.assertj.core.util.Lists;
@@ -8,17 +10,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class ShoppingCartControllerTest {
+public class ShoppingCartControllerTest extends WebApplicationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -34,5 +38,16 @@ public class ShoppingCartControllerTest {
                 .andExpect(jsonPath("$.products").isArray())
                 .andExpect(jsonPath("$.products").isEmpty())
                 .andExpect(jsonPath("$.totalPrice").value(BigDecimal.ZERO));
+    }
+
+    @Test
+    void should_return_2_item() throws Exception {
+        List<Product> products = List.of(new Product(1L, "coke", BigDecimal.valueOf(12.0), 1),
+                new Product(2L, "hershey", BigDecimal.valueOf(15.5), 2));
+        when(shoppingCartService.getShoppingCart()).thenReturn(new ShoppingCartResponse(products, BigDecimal.valueOf(43)));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/shopping-cart").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.products").isArray())
+                .andExpect(jsonPath("$.totalPrice").value(BigDecimal.valueOf(43)));
     }
 }

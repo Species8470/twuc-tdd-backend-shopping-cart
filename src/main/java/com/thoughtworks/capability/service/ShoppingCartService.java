caf2edb7fd75cefabb4be06c31a2b86b5970497c
Zhiqi Lu <zhiqilu8470@gmail.com>
2022-10-12 20:56:53 +0800

    feat: ShoppingCartService - getShoppingCart

--- src/main/java/com/thoughtworks/capability/service/ShoppingCartService.java
+++ src/main/java/com/thoughtworks/capability/service/ShoppingCartService.java
@@ -1,20 +1,28 @@
 package com.thoughtworks.capability.service;
 
+import com.thoughtworks.capability.domain.Product;
 import com.thoughtworks.capability.repository.ProductRepository;
 import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class ShoppingCartService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public ShoppingCartService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     public ShoppingCartResponse getShoppingCart() {
-        return new ShoppingCartResponse(productRepository.findAll(), BigDecimal.ZERO);
+        List<Product> result = productRepository.findAll();
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        for(Product product : result) {
+            BigDecimal temp = BigDecimal.valueOf(product.getPrice().doubleValue() * product.getQuantity());
+            totalPrice = totalPrice.add(temp);
+        }
+        return new ShoppingCartResponse(result, totalPrice);
     }
 }
--- src/test/java/com/thoughtworks/capability/service/ShoppingCartServiceTest.java
+++ src/test/java/com/thoughtworks/capability/service/ShoppingCartServiceTest.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.capability.service;
 
+import com.thoughtworks.capability.domain.Product;
 import com.thoughtworks.capability.repository.ProductRepository;
 import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,14 +18,31 @@ public class ShoppingCartServiceTest {
 
     private final ProductRepository productRepository = mock(ProductRepository.class);
 
+    private ShoppingCartService shoppingCartService;
+
+    @BeforeEach
+    void setUp() {
+        shoppingCartService = new ShoppingCartService(productRepository);
+    }
+
     @Test
     void should_return_empty_shopping_cart() {
         when(productRepository.findAll()).thenReturn(emptyList());
-        ShoppingCartService shoppingCartService = new ShoppingCartService(productRepository);
 
         ShoppingCartResponse response = shoppingCartService.getShoppingCart();
 
         assertEquals(0, response.getProducts().size());
         assertEquals(BigDecimal.ZERO, response.getTotalPrice());
+    }
+
+    @Test
+    void should_return_shopping_cart_with_2_items() {
+        List<Product> products = List.of(new Product(1L, "coke", BigDecimal.valueOf(12.0), 1),
+                new Product(2L, "hershey", BigDecimal.valueOf(15.5), 2));
+        when(productRepository.findAll()).thenReturn(products);
+
+        ShoppingCartResponse response = shoppingCartService.getShoppingCart();
+
+        assertEquals(response.getTotalPrice(), BigDecimal.valueOf(43.0));
     }
 }
